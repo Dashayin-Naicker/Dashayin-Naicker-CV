9617dee40a9dc706751cd9baf050059cdc754cea
Dashayin Naicker <dashayinnaicker@gmail.com>
2025-03-30 14:40:49 +0200

    Even more updated CV

--- obj/Debug/net9.0/Dashayin_Naicker_CV.AssemblyInfo.cs
+++ obj/Debug/net9.0/Dashayin_Naicker_CV.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dashayin_Naicker_CV")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+871a61680742a36199f8d4e6f1d94795ececd644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4031cca566b596ccf6ca8a108d2f2a8e81cd12a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dashayin_Naicker_CV")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dashayin_Naicker_CV")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
